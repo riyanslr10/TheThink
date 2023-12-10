90edfcf85371256630e12fb19a1539eb19cbe7a7
riyanslr10 <riyan.putras40@gmail.com>
2023-12-11 04:16:36 +0700

    update git

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="bg-blue-200 shadow-lg">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-center">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+<nav class="bg-blue-200 shadow-lg fixed">
+    <div class=" flex flex-wrap items-center justify-between px-[1px] py-3 text-center ">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse px-3">
             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-blue-900">TheThink</span>
         </a>
-        <div class="flex md:order-2">
+        <div class="flex md:order-2 pl-24">
             <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
                 aria-expanded="false"
                 class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
@@ -38,7 +38,7 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div class="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse pl-5">
+            <div class="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse px-5 ">
                 <a href="/login"
                     class="px-12 py-2 text-sm font-medium text-center text-blue-200 bg-blue-900 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Sign
                     up / Sign in</a>
