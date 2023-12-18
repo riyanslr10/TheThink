3f1aee07d5c483faf9099ccc31349d5a24e5f777
riyanslr10 <riyan.putras40@gmail.com>
2023-12-18 13:40:54 +0700

    navbar dropdownv2

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -43,7 +43,7 @@
                     <li x-data="{ isOpen: false }" @click.away="isOpen = false" class="relative nav-item dropdown px-5">
                         
                         <a @click="isOpen = !isOpen" class="nav-link dropdown-toggle cursor-pointer" role="button" aria-haspopup="true" :aria-expanded="isOpen ? 'true' : 'false'">
-                            Welcome, {{ auth()->user()->name }} <svg class="ml-10 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                            Welcome, {{ auth()->user()->name }} <svg class="ml-10 w-6 h-6  text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
                               </svg>
                         </a>
