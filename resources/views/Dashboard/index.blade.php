5d54c96a06488851ce9fe7f7ef32552817f7d2fc
riyanslr10 <riyan.putras40@gmail.com>
2023-12-31 23:42:06 +0700

    finalisasi

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -32,31 +32,35 @@
                </div> --}}
 
             <!-- Jika pengguna adalah admin -->
-            {{-- @can('admin')
-    <!-- Konten lainnya yang hanya tampil jika pengguna adalah admin -->
-    <div class="grid grid-cols-3 gap-4 mb-4">
-        <!-- Jumlah pengguna (users) -->
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
-                {{ \App\Models\User::count() }} Users
-            </p>
-        </div>
-        <!-- Jumlah kategori (categories) -->
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
-                {{ \App\Models\Category::count() }} Categories
-            </p>
-        </div>
-        <!-- Jumlah postingan (posts) untuk setiap kategori -->
-        @foreach ($categories as $category)
-            <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
-                    {{ $category->name }}: {{ $category->posts->count() }} Posts
-                </p>
-            </div>
-        @endforeach
-    </div>
-@endcan --}}
+            @can('admin')
+                <!-- Konten lainnya yang hanya tampil jika pengguna adalah admin -->
+                <div class="grid grid-cols-3 gap-4 mb-4">
+
+                    <!-- Jumlah pengguna (users) -->
+                    <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                        <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
+                            {{ \App\Models\User::count() }} Users
+                        </p>
+                    </div>
+
+                    <!-- Jumlah kategori (categories) -->
+                    <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                        <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
+                            {{ \App\Models\Category::count() }} Categories
+                        </p>
+                    </div>
+
+                    <!-- Jumlah postingan (posts) untuk setiap kategori -->
+                    @foreach (\App\Models\Category::all() as $category)
+                        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p class="text-2xl font-extrabold text-gray-800 dark:text-gray-500">
+                                {{ $category->name }}: {{ $category->posts->count() }} Posts
+                            </p>
+                        </div>
+                    @endforeach
+
+                </div>
+            @endcan
 
 
 
@@ -81,56 +85,4 @@
             @endif
 
 
-            <div class="grid grid-cols-2 gap-4 mb-4">
-                <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white
-                    ms-4" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4" />
-                    </svg>
-                    <p class="text-xl font-extrabold text-gray-800 dark:text-gray-500 ml-2 me-2">
-                        <a href="/dashboard/posts"> {{ auth()->user()->name }} memiliki {{ auth()->user()->posts->count() }}
-                            Posts Books</a>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white
-                    ms-4" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4" />
-                    </svg>
-                    <p class="text-xl font-extrabold text-gray-800 dark:text-gray-500 ml-2 me-2">
-                        <a href="/dashboard/posts"> {{ auth()->user()->name }} memiliki {{ auth()->user()->posts->count() }}
-                            Posts Books</a>
-                    </p>
-                </div>
-
-                <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white
-                    ms-4" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4" />
-                    </svg>
-                    <p class="text-xl font-extrabold text-gray-800 dark:text-gray-500 ml-2 me-2">
-                        <a href="/dashboard/posts"> {{ auth()->user()->name }} memiliki {{ auth()->user()->posts->count() }}
-                            Posts Books</a>
-                    </p>
-                </div>
-
-                <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white
-                    ms-4" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4" />
-                    </svg>
-                    <p class="text-xl font-extrabold text-gray-800 dark:text-gray-500 ml-2 me-2">
-                        <a href="/dashboard/posts"> {{ auth()->user()->name }} memiliki {{ auth()->user()->posts->count() }}
-                            Posts Books</a>
-                    </p>
-                </div>
-            </div>
         @endsection
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,28 +1,28 @@
-	@extends('layouts.main')
+ @extends('layouts.main')
 
-@section('container')
-    <div class="container pt-28 md:pt-28">
-        <h1 class="text-3xl font-bold text-center text-stone-600 mb-7   px-6 md:mx-20 lg:mx-60">
-            Select the category you are interested in
-        </h1>
-        <div
-            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center px-4 md:px-10 lg:px-20 pb-10 me-4 ms-4">
-            @foreach ($categories as $category)
-                {{-- category --}}
-                <div
-                    class="w-full sm:w-[300px]  border border-orange-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-8">
-                    <div class="flex flex-col">
-                        <img class="object-cover w-full h-[200px] rounded-t-lg shadow-lg hover:scale-105 transition-transform duration-300"
-                            src="https://source.unsplash.com/500x500?{{ $category->name }}" alt="{{ $category->name }}" />
-                        <div class="flex justify-center pt-5">
-                            <a href="/posts?category={{ $category->slug }}"
-                                class="w-full px-6 py-2 text-lg font-bold text-center text-stone-600 bg-orange-300 bg-opacity-80 hover:bg-orange-400 rounded-lg focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-300 dark:hover:bg-orange-400 dark:focus:ring-orange-400 shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                                {{ $category->name }}
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-@endsection
+ @section('container')
+     <div class="container ">
+         <h1 class="text-3xl font-bold text-center text-stone-600 mb-7   pb-6 md:mx-20 lg:mx-60">
+             Select the category you are interested in
+         </h1>
+         <div
+             class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center px-4 md:px-10 lg:px-20 pb-10 me-10 ms-10">
+             @foreach ($categories as $category)
+                 {{-- category --}}
+                 <div
+                     class="w-full sm:w-[300px]  border border-orange-400 rounded-lg  dark:bg-gray-800 dark:border-gray-700 mb-8">
+                     <div class="flex flex-col">
+                         <img class="object-cover w-full h-[200px] rounded-t-lg  "
+                             src="https://source.unsplash.com/500x500?{{ $category->name }}" alt="{{ $category->name }}" />
+                         <div class="flex justify-center pt-1">
+                             <a href="/posts?category={{ $category->slug }}"
+                                 class="w-full px-6 py-2 text-xl font-bold text-center text-amber-700  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                                 {{ $category->name }}
+                             </a>
+                         </div>
+                     </div>
+                 </div>
+             @endforeach
+         </div>
+     </div>
+ @endsection
